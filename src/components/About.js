7a1c04197f95c6eecce2d38307a1da8acc7ee768
Shareef Rossetti <shareefrossetti@gmail.com>
2023-07-03 20:37:05 -0400

    changed about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ import React from "react";
 function About() {
   return (
     <div>
-      <p className="content is-medium">Super Hero/Model and soon to be Full Stack Developer</p>
+      <p className="content is-medium">Full Stack Developer Who Loves to Build Fantastic Applications</p>
       <hr />
-      <img className="my-pic" src={process.env.PUBLIC_URL + '/img/profile.jpg'} alt="Emily Necciai Mayeski"/>
+      <img className="my-pic" src={process.env.PUBLIC_URL + '/img/profile.jpg'} />
       <p className="content is-italic mt-4">
-        I'm Shareef, an awesome dude studying the full stack to be the best coder on the planet!
+        I'm Shareef, a Software Engineering aspiring to the best coder on the planet!
       </p>
       <p className="content">
        I enjoy playing sports and I like to create cool apps to better the world.
